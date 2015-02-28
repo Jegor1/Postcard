323123d77eeacfd3659d6d58daf8ae2fec003e06
Jegor <jegor@outlook.com>
2015-02-28 15:18:33 +0100

    another commit

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
     
 
     override func viewDidLoad() {
@@ -37,7 +38,16 @@ class ViewController: UIViewController {
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
         
+        nameLabel.text = enterNameTextField.text
+        nameLabel.hidden = false
+        
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
+        
         mailButton.setTitle("Mail sent", forState: UIControlState.Normal)
+        mailButton.backgroundColor = UIColor.blueColor()
+        mailButton.tintColor = UIColor.greenColor()
+        
     }
 
 }
